f4ef571728339ad9740ffc674643c6143a13aa60
Jullious Mattheson <87356775+Julz876@users.noreply.github.com>
2024-08-21 23:29:13 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,17 @@
 import bpy
-from .main_script import register as main_register, unregister as main_unregister
+from .main import register as main_register, unregister as main_unregister
 
 bl_info = {
     "name": "UE Texture Importer",
+    "description": "Imports Unreal Engine JSON materials and automatically sets up corresponding textures in Blender.",
+    "author": "KingxJulz",
     "version": (1, 0, 0),
     "blender": (3, 0, 0),
+    "location": "View3D > Sidebar > PSK / PSA",
+    "warning": "This requires the PSK / PSA addon in the N-Panel/Sidebar",
+    "wiki_url": "https://github.com/Julz876/FModel-Auto-Texture/blob/main/README.md",
+    "tracker_url": "https://github.com/Julz876/FModel-Auto-Texture/issues",
     "category": "Import-Export",
-    "author": "KingJulz",
 }
 
 def register():
